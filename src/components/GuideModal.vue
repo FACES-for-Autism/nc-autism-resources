d5187f2cb1fadd2aa3412e7cc954e3be5c0ad299
Walt Gurley <walt.gurley@gmail.com>
2022-02-10 10:30:39 -0500

    styled and fixed position of guide modal

--- src/components/GuideModal.vue
+++ src/components/GuideModal.vue
@@ -1,10 +1,10 @@
 <template>
-<div>
+<div class="absolute left-0 top-0">
   <div 
     class="fixed z-40 left-0 top-0 w-screen h-screen bg-gray-300 bg-opacity-70"
     @click="closeModal"
   ></div>
-  <div class="modal z-50 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10/12 p-4 border border-gray-800 bg-white">
+  <div class="modal z-50 fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 sm:w-10/12 lg:w-8/12 max-w-2xl p-4 border border-gray-800 bg-white">
     <button
       class="absolute right-4 top-4"
       aria-label="Close guide"
@@ -18,7 +18,7 @@
     <h2>About...</h2>
     <p>This is what people need to know to use this application</p>
 
-    <a class="font-semibold text-black" href="https://sites.google.com/ncsu.edu/facesprogram/contact-us">Contact FACES</a>
+    <a class="fixed bottom-4 left-0 right-0 text-center font-semibold text-black" href="https://sites.google.com/ncsu.edu/facesprogram/contact-us">Contact FACES</a>
   </div>
 </div>
 </template>
